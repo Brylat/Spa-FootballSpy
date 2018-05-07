5563dd95d50864e89a4e89f64d16923ef1f4e480
Brylat <brylat8@hotmail.com>
2018-05-07 18:51:56 +0200

    Add missing route

--- src/app/after-login/after-login-routing.module.ts
+++ src/app/after-login/after-login-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AfterLoginComponent } from './after-login.component';
 import { LeaguesComponent } from './leagues/leagues.component';
+import { LeagueDetailsComponent } from './league-details/league-details.component';
 
 const routes: Routes = [
   {
     path: '', component: AfterLoginComponent, children: [
-      { path: 'leagues', component: LeaguesComponent }
+      { path: 'leagues', component: LeaguesComponent },
+      { path: 'league/:id', component: LeagueDetailsComponent }
     ]
   }
 ];
