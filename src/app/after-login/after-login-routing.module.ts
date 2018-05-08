f033b519d4c3fc7a7e3baac5e398bc9405c0835d
adian <adrian.scehura@gmail.com>
2018-05-08 23:18:29 +0200

    add league/:id to router

--- src/app/after-login/after-login-routing.module.ts
+++ src/app/after-login/after-login-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AfterLoginComponent } from './after-login.component';
 import { LeaguesComponent } from './leagues/leagues.component';
 import { AuthGuard } from '../shared/auth/auth.guard';
+import { LeagueDetailsComponent } from './league-details/league-details.component';
 
 const routes: Routes = [
   {
     path: '', component: AfterLoginComponent, canActivate: [AuthGuard], children: [
-      { path: 'leagues', component: LeaguesComponent }
+      { path: 'leagues', component: LeaguesComponent },
+      { path: 'league/:id', component: LeagueDetailsComponent }
     ]
   }
 ];
