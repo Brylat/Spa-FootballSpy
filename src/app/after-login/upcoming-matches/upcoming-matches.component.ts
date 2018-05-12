d018f03e4bf3a2bd7a1daf9d6ffddc1d121365e7
ChuZZZta <bodzio223@gmail.com>
2018-05-12 15:39:55 +0200

    Updating layout for upcoming matches

--- src/app/after-login/upcoming-matches/upcoming-matches.component.ts
+++ src/app/after-login/upcoming-matches/upcoming-matches.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FootballDataService } from '../../shared/football-data.service';
 import {FormControl } from '@angular/forms';
 import { Match } from '../../shared/objects-template/match';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-upcoming-matches',
@@ -14,8 +15,12 @@ export class UpcomingMatchesComponent implements OnInit {
   public Matches = [];
   public Countries = new FormControl('');
   public League = new FormControl('');
-  constructor(private footballDataService: FootballDataService) { }
 
+  public dataSource;
+  public displayedColumns = ['schedule_date', 'stadium', 'team_season_home_name', 'team_season_away_name'];
+
+
+  constructor(private footballDataService: FootballDataService) { }
 
   public ChangeSelectedCountry(value) {
     this.footballDataService.GetLeaguesByCountryId(value._id).subscribe(data => {
@@ -26,10 +31,14 @@ export class UpcomingMatchesComponent implements OnInit {
     this.footballDataService.GetAllSeasonsByLeagueId(value._id).subscribe(seasons => {
       this.footballDataService.GetUpcomingBySeasonId(seasons.docs[seasons.docs.length - 1]._id).subscribe(data => {
         this.Matches = data.docs;
+        this.dataSource = new MatTableDataSource(data.docs);
       });
     });
   }
-
+  public ClickedRow(row) {
+    alert(row.id_team_season_away + ' ' + row.id_team_season_home);
+    // this.router.navigate(['/team/' + row.id_team_season]);
+  }
   ngOnInit() {
     this.footballDataService.GetAllCountry().subscribe(data => {
       this.AllCountries = data.docs;
