8510b35c94d86fad719921e2fb38385bc359a47f
ChuZZZta <bodzio223@gmail.com>
2018-05-12 13:49:44 +0200

    Adding logic for upcoming matches component

--- src/app/after-login/upcoming-matches/upcoming-matches.component.ts
+++ src/app/after-login/upcoming-matches/upcoming-matches.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FootballDataService } from '../../shared/football-data.service';
+import {FormControl } from '@angular/forms';
+import { Match } from '../../shared/objects-template/match';
 
 @Component({
   selector: 'app-upcoming-matches',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upcoming-matches.component.css']
 })
 export class UpcomingMatchesComponent implements OnInit {
+  public AllCountries = [];
+  public Leagues = [];
+  public Matches = [];
+  public Countries = new FormControl('');
+  public League = new FormControl('');
+  constructor(private footballDataService: FootballDataService) { }
 
-  constructor() { }
+
+  public ChangeSelectedCountry(value) {
+    this.footballDataService.GetLeaguesByCountryId(value._id).subscribe(data => {
+      this.Leagues = data.docs;
+    });
+  }
+  public ChangeSelectedLeague(value) {
+    this.footballDataService.GetAllSeasonsByLeagueId(value._id).subscribe(seasons => {
+      this.footballDataService.GetUpcomingBySeasonId(seasons.docs[seasons.docs.length - 1]._id).subscribe(data => {
+        this.Matches = data.docs;
+      });
+    });
+  }
 
   ngOnInit() {
+    this.footballDataService.GetAllCountry().subscribe(data => {
+      this.AllCountries = data.docs;
+    });
   }
 
 }
